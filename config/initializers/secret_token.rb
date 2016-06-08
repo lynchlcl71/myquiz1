b27618beb0080190c9e7ad17dcaa6216557c35a1
vagrant <vagrant@precise32.(none)>
2016-06-08 21:38:59 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myquiz1::Application.config.secret_key_base = '2510a2a2252fe77e9acdecc4c3bda0ab6a7b3c9b7c9b5dc598dca6824085e58b8361bdf64b88ae03df4ac2647c8cad9eb78518178e6df18291a997f2ecf40fb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myquiz1::Application.config.session_store :cookie_store, key: '_myquiz1_session'
